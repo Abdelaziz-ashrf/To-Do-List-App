28402bbe6cb16947ba2f5cd3cdd46a179ea63478
Abdelaziz-ashrf <abdelazizballouz@gmail.com>
2023-08-14 10:18:37 +0200

    To-Do List App

---
+++ js/main.js
@@ -0,0 +1,39 @@
+let inputBox = document.getElementById("input-box")
+let listContainer = document.getElementById("list-container")
+
+function addTask(){
+    let regex = /^\s+$/ig;
+    if(inputBox.value.match(regex) || inputBox.value.length == ``){
+        alert('You Should type something!');
+    }else {
+        let li = document.createElement('li');
+        li.innerHTML = inputBox.value;
+        let span = document.createElement('span');
+        span.innerHTML = '\u00d7'
+        li.appendChild(span)
+        listContainer.appendChild(li)
+    }
+    inputBox.innerHTML =``
+    saveData()
+}
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === 'LI'){
+        e.target.classList.toggle("checked")
+        saveData()
+    }else if (e.target.tagName === "SPAN"){
+        e.target.parentElement.remove()
+        saveData()
+    }
+}, false)
+
+
+function saveData(){
+    localStorage.setItem('data', listContainer.innerHTML)
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem('data')
+}
+
+showTask()
